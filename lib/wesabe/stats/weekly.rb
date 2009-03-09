54b0b1efe9f7ec91e7a9f8cb493de0fdfefaa425
mhat <matt.knopp@gmail.com>
2009-03-09 16:25:05 -0700

    added weekly totals

--- lib/wesabe/stats/weekly.rb
+++ lib/wesabe/stats/weekly.rb
@@ -41,19 +41,18 @@ module Wesabe::Stats
     end
     
     def each_week (&block)
-      @weeks.each do |week|
-        @tags.each_value do |tag|
-          tag_wk_total = 0.0
-          tag_wk_count = 0
-          tag.txactions.each do |txaction|
-            if txaction.date > week[0] && txaction.date < week[1]
-              tag_wk_total += txaction.amount
-              tag_wk_count += 1
-            end
-          end
-          
-          block.call(week,tag,tag_wk_total,tag_wk_count)
+      totals = Array.new(weeks.size,0)
+      counts = Array.new(weeks.size,0)
+      
+      @tags.each_value do |tag|
+        tag.each_with_index do |arr,idx|
+          totals[idx] += arr[1]
+          counts[idx] += arr[2]
         end
+      end
+      
+      (0..weeks.size-1).each do |idx|
+        block.call(weeks[idx], totals[idx], counts[idx])
       end
     end
   end
